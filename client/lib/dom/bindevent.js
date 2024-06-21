2eae54becb18d838b62fedf11db3e82d96be12b0
vngkt3 <wkdalsgur456@naver.com>
2024-06-22 02:52:49 +0900

    6/22

---
+++ client/lib/dom/bindevent.js
@@ -0,0 +1,7 @@
+function bindEvent(node, type, handler) {
+  if (isString(node)) node = getNode(node);
+
+  node.addEventListener(type, handler);
+
+  return () => node.removeEventListener(type, handler);
+}
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,33 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+const ignore = {
+  isString: true,
+  isObject: true,
+  isArray: true,
+  isNull: true,
+  getNode: true,
+  insertLast: true,
+  attr: true,
+  css: true,
+  bindEvent: true,
+  addClass: true,
+  removeClass: true,
+};
 
 export default [
   pluginJs.configs.recommended,
-  { 
-    languageOptions: { 
-      globals: { 
+  {
+    languageOptions: {
+      globals: {
         ...globals.browser,
-        ...globals.node } 
-        } ,
-        rules: {
-          "no-unused-vars":"warn"
-        }
-        
+        ...globals.node,
+        ...ignore,
       },
+    },
 
+    rules: {
+      'no-unused-vars': 'warn',
+    },
+  },
 ];
